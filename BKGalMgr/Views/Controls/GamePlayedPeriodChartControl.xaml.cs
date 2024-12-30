bdaf68761e7e864fd20a03982b98d0717f53ef1e
bluelaze <bluelaze@users.noreply.github.com>
2024-12-31 00:34:16 +0800

    feat: 游玩页面支持时间图表

--- BKGalMgr/Views/Controls/GamePlayedPeriodChartControl.xaml.cs
+++ BKGalMgr/Views/Controls/GamePlayedPeriodChartControl.xaml.cs
@@ -4,6 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using BKGalMgr.ViewModels;
+using LiveChartsCore;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -11,6 +15,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using SkiaSharp;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -27,8 +32,93 @@ public sealed partial class GamePlayedPeriodChartControl : UserControl
         set { SetValue(PlayedPeriodsProperty, value); }
     }
     public static readonly DependencyProperty PlayedPeriodsProperty = DependencyProperty.Register(
-        "PlayedPeriods",
+        nameof(PlayedPeriods),
         typeof(IEnumerable<PlayedPeriodInfo>),
+        typeof(GamePlayedPeriodChartControl),
+        new PropertyMetadata(
+            null,
+            (DependencyObject o, DependencyPropertyChangedEventArgs args) =>
+            {
+                var chart = (GamePlayedPeriodChartControl)o;
+                var playedPeriods = (IEnumerable<PlayedPeriodInfo>)args.NewValue;
+                if (playedPeriods == null)
+                {
+                    chart.XAxes = null;
+                    chart.YAxes = null;
+                }
+                else
+                {
+                    SKColor labelColor = new SKColor(128, 128, 128);
+                    if (chart.Foreground is SolidColorBrush colorBrush)
+                        labelColor = new SKColor(
+                            colorBrush.Color.R,
+                            colorBrush.Color.G,
+                            colorBrush.Color.B,
+                            colorBrush.Color.A
+                        );
+
+                    chart.XAxes = new List<ICartesianAxis>()
+                    {
+                        new Axis()
+                        {
+                            LabelsPaint = new SolidColorPaint(labelColor),
+                            TextSize = 14,
+                            Labels = playedPeriods
+                                .Select(t => t.Coordinate.SecondaryValue.AsDate().ToString())
+                                .Reverse()
+                                .ToList(),
+                        },
+                    };
+
+                    chart.YAxes = new List<ICartesianAxis>()
+                    {
+                        new Axis
+                        {
+                            LabelsPaint = new SolidColorPaint(labelColor),
+                            TextSize = 14,
+                            SeparatorsPaint = new SolidColorPaint(SKColors.Gray.WithAlpha(100)),
+                            Labeler = value => value.AsTimeSpan().Format("hhmmss"),
+                        },
+                    };
+                }
+            }
+        )
+    );
+
+    public Visibility ShowClose
+    {
+        get { return (Visibility)GetValue(ShowCloseProperty); }
+        set { SetValue(ShowCloseProperty, value); }
+    }
+    public static readonly DependencyProperty ShowCloseProperty = DependencyProperty.Register(
+        nameof(ShowClose),
+        typeof(Visibility),
+        typeof(GamePlayedPeriodChartControl),
+        new PropertyMetadata(Visibility.Visible)
+    );
+
+    public IEnumerable<ICartesianAxis> XAxes
+    {
+        get => (IEnumerable<ICartesianAxis>)GetValue(XAxesProperty);
+        set => SetValue(XAxesProperty, value);
+    }
+
+    public IEnumerable<ICartesianAxis> YAxes
+    {
+        get => (IEnumerable<ICartesianAxis>)GetValue(YAxesProperty);
+        set => SetValue(YAxesProperty, value);
+    }
+
+    public static readonly DependencyProperty XAxesProperty = DependencyProperty.Register(
+        nameof(XAxes),
+        typeof(IEnumerable<ICartesianAxis>),
+        typeof(GamePlayedPeriodChartControl),
+        new PropertyMetadata(null)
+    );
+
+    public static readonly DependencyProperty YAxesProperty = DependencyProperty.Register(
+        nameof(YAxes),
+        typeof(IEnumerable<ICartesianAxis>),
         typeof(GamePlayedPeriodChartControl),
         new PropertyMetadata(null)
     );
