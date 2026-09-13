50adf68cfce797c6c9689d94ebe79b201bc720dd
bluelaze <bluelaze@users.noreply.github.com>
2026-09-14 01:22:37 +0800

    feat: 游戏原画、声优等列表信息支持粘贴时以空格拆分添加

--- BKGalMgr/Views/Controls/GameInfoControl.xaml.cs
+++ BKGalMgr/Views/Controls/GameInfoControl.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using BKGalMgr.Helpers;
 using BKGalMgr.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -12,6 +13,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -47,6 +49,31 @@ public sealed partial class GameInfoControl : UserControl
             //queryButton.Visibility = Visibility.Collapsed;
             queryButton.Opacity = 0;
             queryButton.Width = 0;
+        }
+        if (ele?.FindDescendant<TextBox>() is TextBox innerTextBox)
+        {
+            var flyout = new MenuFlyout();
+            flyout.Items.Add(new MenuFlyoutItem { Command = new StandardUICommand(StandardUICommandKind.Paste) });
+
+            var customPasteItem = new MenuFlyoutItem
+            {
+                Text = LanguageHelper.GetString("Commom_MenuItem_PasteWithSpaceSplite/Text"),
+            };
+            customPasteItem.Click += async (object sender, RoutedEventArgs e) =>
+            {
+                var dataPackageView = Clipboard.GetContent();
+                if (dataPackageView.Contains(StandardDataFormats.Text))
+                {
+                    string text = await dataPackageView.GetTextAsync();
+                    if (!string.IsNullOrWhiteSpace(text))
+                    {
+                        innerTextBox.Text = string.Join(',', text.Split(' ')) + ",";
+                    }
+                }
+            };
+
+            flyout.Items.Add(customPasteItem);
+            innerTextBox.ContextFlyout = flyout;
         }
     }
 
